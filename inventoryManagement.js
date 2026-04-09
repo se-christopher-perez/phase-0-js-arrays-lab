0aaa0f67160a77a7571c6bbeabefaab5c1ef3c8a
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-04-09 12:33:10 -0400

    Added the four functions!

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,33 @@
 // Write your code here
 
+const products = ["Laptop", "Phone", "Headphones", "Monitor"]
 
+function logFirstProduct() {
+  console.log(products[0])
+}
+
+function addProduct (new_product) {
+  products.push(new_product)  
+  return products
+}
+
+function updateProductName (position, new_name) {
+  products[position] = new_name
+  return products
+}
+
+function removeLastProduct () {
+  products.pop()
+  return products
+}
+
+console.log(logFirstProduct())
+
+console.log(addProduct("Fridge"))
+
+console.log(updateProductName(4, "Keyboard"))
+
+console.log(removeLastProduct())
 
 // Export the necessary parts for testing
 module.exports = {
